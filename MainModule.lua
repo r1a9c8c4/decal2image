a82a5843c594068fe6be17dbe643d945ca14ea43
r1a9c8c4 <37939506+r1a9c8c4@users.noreply.github.com>
2021-07-08 00:37:35 -0500

    Update MainModule.lua

--- MainModule.lua
+++ MainModule.lua
@@ -7,12 +7,9 @@ return function(id)
 	local location = HttpService:JSONDecode(asset).location
 	-- Send a request to the XML file's URL (returns XML as string)
 	local xml = HttpService:GetAsync(location)
-	-- Tries to find the url property inside of the XML by searching for the start and end
-	local urlStart = string.find(xml, "<url>")
-	local urlEnd = string.find(xml, "</url>")
-	-- Crop the string between the two url attributes
-	local url = string.sub(xml, urlStart + 5, urlEnd - 1)
-	-- Get the id by splitting the equal sign and getting the latter half
+	-- Get the url by matching
+	local url = xml:match("<url>(.-)</url>")
+	-- Get the id by splitting the equal sign and getting the ladder half
 	local result = string.split(url, "=")[2]
  
 	return result
