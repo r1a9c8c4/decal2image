fe1dceee646075df68e226e506a7f31c688ffabc
Ty <74572291+TyNoOutlet@users.noreply.github.com>
2021-07-08 16:24:16 -0400

    MainModule.lua
    oops haha kinda new to this typing stuff

--- MainModule.lua
+++ MainModule.lua
@@ -3,10 +3,10 @@
 local httpService = game:GetService("HttpService")
 
 -- Luau typing
-return function(id: number): (boolean?, string?)
+return function(id: number): (boolean | string?)
 	-- Get the xml
 	local success, xml = pcall(httpService.GetAsync, httpService, string.format("https://assetdelivery.rprxy.xyz/v1/asset?id=%i", id))
 
 	-- Returns the id if it was a success, otherwise return false
-	return success, string.match(xml, "?id=(.-)</url>")
+	return success and string.match(xml, "?id=(.-)</url>")
 end
