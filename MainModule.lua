6413a548f2ecc14470567320df78a64f7cab2ee4
r1a9c8c4 <37939506+r1a9c8c4@users.noreply.github.com>
2021-07-08 14:54:38 -0500

    Update MainModule.lua

--- MainModule.lua
+++ MainModule.lua
@@ -1,10 +1,10 @@
 local httpService = game:GetService("HttpService")
 
 -- Luau typing
-return function(id: number): (string,boolean)
+return function(id: number): (string, boolean)
 	-- Get the xml
-	local success,xml = pcall(httpService.GetAsync,httpService,string.format("https://assetdelivery.rprxy.xyz/v1/asset?id=%i", id))
+	local success,xml = pcall(httpService.GetAsync, httpService, string.format("https://assetdelivery.rprxy.xyz/v1/asset?id=%i", id))
 
 	-- Returns the id if it was a success, otherwise return false
-	return success and string.match(xml,"?id=(.-)</url>")
+	return success and string.match(xml, "?id=(.-)</url>")
 end
