c38b9314b2804ca4551888016510556a843a8418
Ty <74572291+TyNoOutlet@users.noreply.github.com>
2021-12-18 14:50:53 -0500

    Replace rprxy with RoProxy

--- MainModule.lua
+++ MainModule.lua
@@ -5,7 +5,7 @@ local httpService = game:GetService("HttpService")
 -- Luau typing
 return function(id: number): (boolean | string?)
 	-- Get the xml
-	local success, xml = pcall(httpService.GetAsync, httpService, string.format("https://assetdelivery.rprxy.xyz/v1/asset?id=%i", id))
+	local success, xml = pcall(httpService.GetAsync, httpService, string.format("https://assetdelivery.roproxy.com/v1/asset?id=%i", id))
 
 	-- Returns the id if it was a success, otherwise return false
 	return success and string.match(xml, "?id=(.-)</url>")
